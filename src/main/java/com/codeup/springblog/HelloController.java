949c50192dbbba1949850c756dc95c2f201991d6
Walter Smith <walter.c.smith01@gmail.com>
2021-03-25 13:19:57 -0400

    Feat:Passing data to views. On the HelloController, a get method was used as well as a path variable and a model to add pieces of data to the model that can then be displayed to the HTML.

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -9,17 +9,17 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String sayHello(){
-        return "Hello from Spring";
-    }
+//    @GetMapping("/hello")
+//    @ResponseBody
+//    public String sayHello(){
+//        return "Hello from Spring";
+//    }
 
-    @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String sayHello(@PathVariable String name) {
-        return "Hello " + name + "!";
-    }
+//    @GetMapping("/hello/{name}")
+//    @ResponseBody
+//    public String sayHello(@PathVariable String name) {
+//        return "Hello " + name + "!";
+//    }
 
 //    @RequestMapping("/increment/{number}")
 //    @ResponseBody
@@ -27,17 +27,17 @@ public class HelloController {
 //        return number + " plus one is " + (number + 1) + "!";
 //    }
 
-    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
-    @ResponseBody
-    public String addOne(@PathVariable int number) {
-        return number + " plus one is " + (number + 1) + "!";
-    }
-
-//    @GetMapping("/hello/{name}")
-//    public String sayHello(@PathVariable String name, Model model) {
-//        model.addAttribute("name", name);
-//        return "hello";
+//    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String addOne(@PathVariable int number) {
+//        return number + " plus one is " + (number + 1) + "!";
 //    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
 
 
 }
