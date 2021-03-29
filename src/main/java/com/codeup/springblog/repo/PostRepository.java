5809cc2de58c6d6a5f2b9b25dfb8112414be0d46
Walter Smith <walter.c.smith01@gmail.com>
2021-03-29 10:23:15 -0400

    Feat:Created an index and show html

---
+++ src/main/java/com/codeup/springblog/repo/PostRepository.java
@@ -0,0 +1,18 @@
+package com.codeup.springblog.repo;
+
+import com.codeup.springblog.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByTitle(String titleToSearchFor);
+
+
+    @Query("from Post post where post.body like %:term%")
+    List<Post> searchByBodyLike(@Param("term") String term);
+
+}
