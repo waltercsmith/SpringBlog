bd84afc51565097d38a9a0ef051fdb5dd4984852
Walter Smith <walter.c.smith01@gmail.com>
2021-03-28 20:48:37 -0400

    Created a controller that will hand the logic with the guessing the number

---
+++ src/main/java/com/codeup/springblog/DiceRollController.java
@@ -0,0 +1,11 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class DiceRollController {
+
+
+}
