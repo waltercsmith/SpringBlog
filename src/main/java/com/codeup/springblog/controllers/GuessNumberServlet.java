4c9e2a67f2371b981c91f3b17e558b93f32226da
Walter Smith <walter.c.smith01@gmail.com>
2021-04-03 20:46:20 -0400

    Feat:Added getMapping with responsebody that includes a path variable tat is a int number

--- src/main/java/com/codeup/springblog/controllers/GuessNumberServlet.java
+++ src/main/java/com/codeup/springblog/controllers/GuessNumberServlet.java
@@ -3,6 +3,8 @@ package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GuessNumberServlet {
@@ -10,5 +12,18 @@ public class GuessNumberServlet {
     public String guess(){
         return "roll-dice";
     }
+
+
+    @GetMapping("/roll-dice/{number}")
+    @ResponseBody
+    public String guessNumber(@PathVariable int number){
+
+        return "The number you choose is: " + number;
+    }
+
+
+
+
+
 
 }
