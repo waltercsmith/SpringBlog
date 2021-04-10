97109cff6bbdc3e9648213284a826f97ee7a9f80
Walter Smith <walter.c.smith01@gmail.com>
2021-04-10 11:34:59 -0400

    Feat:Created a controller for the second page of the view

---
+++ src/main/java/com/codeup/springblog/controllers/AnotherPageController.java
@@ -0,0 +1,10 @@
+package com.codeup.springblog.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AnotherPageController {
+
+
+}
