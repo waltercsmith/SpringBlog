92de0b70a79154f69cc183a6c8c397c6d94c6b47
Walter Smith <walter.c.smith01@gmail.com>
2021-04-12 20:44:38 -0400

    Feat:Created a view

--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -1,4 +1,18 @@
 package com.codeup.springblog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/profile")
+    @ResponseBody
+    public String profile(){return "This is the profile page";}
+
+    @PostMapping("/profile")
+    public String profile(){return "profile";}
+
 }
