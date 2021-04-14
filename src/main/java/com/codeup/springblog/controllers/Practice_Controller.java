2c968fd2f7bfa787ee2ae263c49d7a867a621cbf
Walter Smith <walter.c.smith01@gmail.com>
2021-04-13 20:56:32 -0400

    Feat:Added a get mapping a method that has a path variable and a model in the parameters that will act as a placeholder for passing data to the view

--- src/main/java/com/codeup/springblog/controllers/Practice_Controller.java
+++ src/main/java/com/codeup/springblog/controllers/Practice_Controller.java
@@ -1,13 +1,21 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class Practice_Controller {
 
     @GetMapping("/practice")
     public String showFrame(){
+        return "practice/frame";
+    }
+
+    @GetMapping("/practice/{name}")
+    public String addPlaceholder(@PathVariable String name, Model model){
+        model.addAttribute("name", name);
         return "practice/frame";
     }
 }
