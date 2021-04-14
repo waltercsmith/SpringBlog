56b44c7343407f2aff67308172b3069d593ce8b1
Walter Smith <walter.c.smith01@gmail.com>
2021-04-13 20:48:22 -0400

    Feat:Added a Controller with a get mapping uri

---
+++ src/main/java/com/codeup/springblog/controllers/Practice_Controller.java
@@ -0,0 +1,11 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Practice_Controller {
+
+    @GetMapping("/practice")
+
+}
