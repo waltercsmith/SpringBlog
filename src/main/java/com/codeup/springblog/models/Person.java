7868300d92ef749e6f973ddeae3868ad0c19266c
Walter Smith <walter.c.smith01@gmail.com>
2021-04-17 21:06:43 -0400

    Feat:Created a new model called Person

---
+++ src/main/java/com/codeup/springblog/models/Person.java
@@ -0,0 +1,12 @@
+package com.codeup.springblog.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private String gender;
+    private String name;
+    private boolean isStudent;
+    private List<String> courses = new ArrayList<>();
+
+}
