a835fcb1a4d02a7ad872ab1608ef019fb4abd0e0
Walter Smith <walter.c.smith01@gmail.com>
2021-04-30 07:04:58 -0400

    Feat:Created getter and setter for the private Long property

--- src/main/java/com/codeup/springblog/models/Ad.java
+++ src/main/java/com/codeup/springblog/models/Ad.java
@@ -15,7 +15,14 @@ public class Ad {
 
     private String description;
 
+    public Long getId(){
+        return id;
 
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
 
 //    public Long getId() {
 //        return id;
